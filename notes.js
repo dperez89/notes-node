caaf5c84797c77426d2b703ce41c19b90e9a84de
Crescendo Account 1 <crescendoaccount1@Emilys-MacBook-Pro-2.local>
2018-11-21 12:16:37 -0600

    added functionality to the read command to actually fetch notes with a title argument. Also validates if anything is found and returns the appropriate response

--- notes.js
+++ notes.js
@@ -37,7 +37,10 @@ var getAll = () => {
 };
 
 var getNote = title => {
-    console.log("Geting note:", title);
+    var notes = fetchNotes();
+    console.log(notes);
+    var noteRead = notes.filter(note => note.title === title);
+    return noteRead[0];
 };
 
 var removeNote = title => {
@@ -47,9 +50,16 @@ var removeNote = title => {
 
     return notes.length !== filteredNotes.length;
 };
+
+var logNote = (note) => {
+    console.log("--");
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+}
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
